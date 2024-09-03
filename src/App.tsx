94251ab03ce295e84694c3e935c60f88901f6a65
jhonatantav <jhony.tav@gmail.com>
2024-09-03 17:31:18 -0300

    create app

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import '@/index.css';
+import { Button } from './components/ui/button';
+
+export function App() {
+  return (
+    <>
+      <div className="flex justify-center h-screen items-center">
+        <Button>Click me</Button>
+      </div>
+    </>
+  );
+}
---
+++ src/pages/auth/sign.tsx
@@ -0,0 +1,3 @@
+export function Sign() {
+  return <div>sign</div>;
+}
